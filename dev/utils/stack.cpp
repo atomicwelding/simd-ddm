f032490a3dee0f11dde13d2a17b8995b14c7480e
weld <ctfweld@gmail.com>
2023-06-07 05:33:18 +0200

    comment code

--- dev/utils/stack.cpp
+++ dev/utils/stack.cpp
@@ -1,7 +1,7 @@
 #include "stack.hpp"
 
 
-// utilities
+// === UTILS === //
 std::string Stack::current_byte() {
     return std::to_string(this->acq.tellg());
 }
@@ -11,8 +11,20 @@ std::runtime_error Stack::error_reading(const std::string& msg) {
 }
 
 
-// main
+// === "MAIN" METHODS === //
 void Stack::load_next_frame() {
+    /*
+     * This method intend to load the next frame by reading the so-called frame block
+     * and frame block. The new frame is stored in a object and a ref to this object is
+     * maintained in a member variable named current_frame.
+     *
+     * We do sanity checks every time by checking if we're looking at the right CIDs for
+     * each blocks.
+     *
+     * Could be great to see when to delete the previous Frame objects. We do not handle
+     * it yet, cause we will need previous frames to make the calculations.
+     */
+
     uint32_t im_cid, im_bytes;
     uint32_t tk_cid, tk_bytes;
 
@@ -21,11 +33,11 @@ void Stack::load_next_frame() {
     if(im_cid != 0)
         throw this->error_reading("Frame block malformed: cannot read image");
 
-    // read length of frame block
+    // read length of frame block and retrieve the 4 bytes for CID
     this->acq.read(reinterpret_cast<char*>(&im_bytes), 4);
     im_bytes -= 4;
 
-    // load image
+    // load image in a buffer
     char im_data[im_bytes];
     this->acq.read(im_data, im_bytes);
 
@@ -34,31 +46,38 @@ void Stack::load_next_frame() {
     if(tk_cid != 1)
         throw this->error_reading("Tick block malformed: cannot read tick infos");
 
-    // read length of tick block
+    // read length of tick block and retrieve the 4 bytes for CID
     this->acq.read(reinterpret_cast<char*>(&tk_bytes), 4);
     tk_bytes -= 4;
 
-    // load tick infos
+    // load ticks
     uint64_t tk_data;
     this->acq.read(reinterpret_cast<char*>(&tk_data), tk_bytes);
 
+    // craft the new frame and keep the reference to it
     this->current_frame = new Frame(im_bytes, tk_bytes, im_data, tk_data);
 }
 
 Stack::Stack(const std::string& path) {
-    this->acq.open(path, std::ios::binary);
-    uint32_t h_cid = 0, h_length = 0;
+    /*
+     * Constructor. We create an fstream to the file at path location and
+     * retrieve some infos from the custom header.
+     */
 
+    this->acq.open(path, std::ios::binary);
+
+    uint32_t h_cid = 0, h_length = 0;
     this->acq.read(reinterpret_cast<char*>(&h_cid), 4);
     this->acq.read(reinterpret_cast<char*>(&h_length), 4);
 
+    // sanity check
     if(h_cid != 2 || h_length != 19)
         throw this->error_reading("Wrong file format");
 
     // get stride
     this->acq.read(reinterpret_cast<char*>(&this->stride), 2);
 
-    // get encoding
+    // get encoding (types of encoding are defined as #define in header file)
     this->acq.read(reinterpret_cast<char*>(&this->encoding), 1);
     if(this->encoding != Mono12Packed)
         throw this->error_reading("Pixel encoding not implemented yet");
@@ -70,6 +89,7 @@ Stack::Stack(const std::string& path) {
     this->acq.read(reinterpret_cast<char*>(&this->aoi_width), 2);
     this->acq.read(reinterpret_cast<char*>(&this->aoi_height), 2);
 
+    // load the first frame in memory !
     this->load_next_frame();
 }
 

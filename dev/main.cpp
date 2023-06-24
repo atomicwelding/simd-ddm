e518fe71a215c087887ee491e44a6beb4ae291cd
weld <ctfweld@gmail.com>
2023-06-25 00:35:34 +0200

    modified options, specifying encoding is now mandatory

--- dev/main.cpp
+++ dev/main.cpp
@@ -10,17 +10,25 @@ namespace po = boost::program_options;
 int main(int argc, char** argv) {
 
     Options options;
+    bool do_print_help = false;
 
-    // à voir si on précise l'encoding ou pas avec la command line ou juste on le déduit ..
-    // ptetre rajouter un "deduce" en option
+    std::string help =
+"This software intends to perform image processing. \
+List of arguments that can be passed at the command-line:\n\
+        --path, -p: Path to the file containing the signal to process. Defaults to sample.dat. \n\
+        --encoding, -e: Specify the frame encoding. Defaults to Mono12Packed.\n\
+        --normalize, -n: Normalize the signal by averaging all the pixels. Defaults to false.\n\
+        --loadNFrames, -N: Load a fixed number N of frames.\n\
+        --help, -h: Print this help message.";
+
     po::options_description desc("Options");
     desc.add_options()
-        ("path,p", po::value<std::string>(&options.path)->default_value("sample.dat"), "Path to the file to process")
-        ("loadNframes,N", po::value<int>(&options.loadNframes)->default_value(1), "Number of frames to be loaded")
-        ("encoding,e", po::value<std::string>(&options.encoding)->default_value("Mono12Packed"), "Encoding type")
-        ("normalize,n", po::bool_switch(&options.do_normalize), "Normalize signal data");
+        ("path,p", po::value<std::string>(&options.path)->default_value("sample.dat"), "Path to the file containing the signal to process")
+        ("loadNframes,N", po::value<int>(&options.loadNframes)->default_value(1), "Load a fixed number of N frames")
+        ("encoding,e", po::value<std::string>(&options.encoding)->default_value("Mono12Packed"), "Specify the frame encoding")
+        ("normalize,n", po::bool_switch(&options.do_normalize), "Normalize the signal data by averaging all the pixels")
+        ("help,h", po::bool_switch(&do_print_help), "Print out help message");
 
-    // parse
     po::variables_map vm;
     try {
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -28,6 +36,11 @@ int main(int argc, char** argv) {
     } catch (const po::error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
+    }
+
+    if(do_print_help) {
+        std::cout << help << std::endl;
+        return 0;
     }
 
     // recap options

eed8b43cf93cfd9cbddf6cb5bc18239ffc489c1e
weld <ctfweld@gmail.com>
2023-07-04 09:38:59 +0200

    refactor constructor

--- dev/src/includes/stack.cpp
+++ dev/src/includes/stack.cpp
@@ -4,6 +4,64 @@
 #include "stack.hpp"
 #include "utils.hpp"
 
+// === CONSTRUCTORS === //
+template<typename T>
+Stack<T>::Stack(const std::string& path, int encoding, int N, bool do_normalize) {
+    /*
+     * Constructor. We create an fstream to the file at path location and
+     * retrieve some infos from the custom header.
+     *
+     * N corresponds to the number of frames to load into the image buffer.
+     */
+
+    this->acq.open(path, std::ios::binary);
+
+    uint32_t h_cid = 0, h_length = 0;
+    this->acq.read(reinterpret_cast<char*>(&h_cid), 4);
+    this->acq.read(reinterpret_cast<char*>(&h_length), 4);
+
+    // sanity check
+    if(h_cid != 2 || h_length != 19)
+        throw this->error_reading("Wrong file format");
+
+    // get stride
+    this->acq.read(reinterpret_cast<char*>(&this->stride), 2);
+
+    // get encoding (types of encoding are defined as #define in header file)
+    this->acq.read(reinterpret_cast<char*>(&this->encoding), 1);
+
+    // checks
+    if(this->encoding != encoding)
+        throw this->error_reading("Wrong encoding. Please, check out the format of the signal.");
+    if(this->encoding != Mono12Packed)
+        throw this->error_reading("Pixel encoding not implemented yet");
+    // get clock frequency
+
+    this->acq.read(reinterpret_cast<char*>(&this->clock_frequency), 8);
+
+    // get aoi infos ; AOIWidth is measured in pixels
+    this->acq.read(reinterpret_cast<char*>(&this->aoi_width), 2);
+    this->acq.read(reinterpret_cast<char*>(&this->aoi_height), 2);
+
+    this->image_size = this->aoi_width * this->aoi_height;
+    this->len_images_buffer = this->image_size * N;
+
+    this->N_images_buffer = reinterpret_cast<T*>(fftw_malloc(this->len_images_buffer*sizeof(T)));
+    if(this->encoding == Mono12Packed) {
+        this->load_M12P_images(N);
+    }
+
+    if(do_normalize)
+        this->normalize();
+}
+
+template<typename T>
+Stack<T>::~Stack() {
+    fftw_free(this->N_images_buffer);
+    this->acq.close();
+}
+
+
 // === UTILS === //
 template<typename T>
 int Stack<T>::current_byte() {
@@ -86,62 +144,6 @@ void Stack<T>::normalize() {
 
     for(int i = 0; i < len_images_buffer; ++i)
         this->N_images_buffer[i] /= mean;
-}
-
-template<typename T>
-Stack<T>::Stack(const std::string& path, int encoding, int N, bool do_normalize) {
-    /*
-     * Constructor. We create an fstream to the file at path location and
-     * retrieve some infos from the custom header.
-     *
-     * N corresponds to the number of frames to load into the image buffer.
-     */
-
-    this->acq.open(path, std::ios::binary);
-
-    uint32_t h_cid = 0, h_length = 0;
-    this->acq.read(reinterpret_cast<char*>(&h_cid), 4);
-    this->acq.read(reinterpret_cast<char*>(&h_length), 4);
-
-    // sanity check
-    if(h_cid != 2 || h_length != 19)
-        throw this->error_reading("Wrong file format");
-
-    // get stride
-    this->acq.read(reinterpret_cast<char*>(&this->stride), 2);
-
-    // get encoding (types of encoding are defined as #define in header file)
-    this->acq.read(reinterpret_cast<char*>(&this->encoding), 1);
-
-    // checks
-    if(this->encoding != encoding)
-        throw this->error_reading("Wrong encoding. Please, check out the format of the signal.");
-    if(this->encoding != Mono12Packed)
-        throw this->error_reading("Pixel encoding not implemented yet");
-    // get clock frequency
-
-    this->acq.read(reinterpret_cast<char*>(&this->clock_frequency), 8);
-
-    // get aoi infos ; AOIWidth is measured in pixels
-    this->acq.read(reinterpret_cast<char*>(&this->aoi_width), 2);
-    this->acq.read(reinterpret_cast<char*>(&this->aoi_height), 2);
-
-    this->image_size = this->aoi_width * this->aoi_height;
-    this->len_images_buffer = this->image_size * N;
-
-    this->N_images_buffer = reinterpret_cast<T*>(fftw_malloc(this->len_images_buffer*sizeof(T)));
-    if(this->encoding == Mono12Packed) {
-            this->load_M12P_images(N);
-    }
-
-    if(do_normalize)
-        this->normalize();
-}
-
-template<typename T>
-Stack<T>::~Stack() {
-    fftw_free(this->N_images_buffer);
-    this->acq.close();
 }
 
 
